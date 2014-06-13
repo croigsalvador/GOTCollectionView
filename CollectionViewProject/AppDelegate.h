5c891fe30eea4f505d1a0a8ef0adf5c7064b1f9c
Carlos Roig Salvador <carroisal3@gmail.com>
2014-06-13 09:52:56 +0200

    Initial Commit

---
+++ CollectionViewProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewProject
+//
+//  Created by Carlos Roig Salvador on 13/06/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewProject
+//
+//  Created by Carlos Roig Salvador on 13/06/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
